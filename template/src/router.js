b6f0c5f9a696170191667c7e0a1d227bc77b2eed
Wong Jiang Fung <kakarukeys@gmail.com>
2016-12-31 01:55:00 +0800

    bolted in bootstrap able to use SCSS able to change title dynamically

--- template/src/router.js
+++ template/src/router.js
@@ -1,10 +1,17 @@
+import _ from 'underscore'
 import VueRouter from 'vue-router'
 import Home from './components/Home'
 import Project from './components/Project'
 
-export default new VueRouter({
+let router = new VueRouter({
   routes: [
-    { path: '/', name: 'home', component: Home },
-    { path: '/projects/:projectName', name: 'project', component: Project }
+    {path: '/', name: 'home', component: Home, meta: {title: 'Home'}},
+    {path: '/projects/:projectName', name: 'project', component: Project, meta: {title: 'Project - <%= projectName %>'}}
   ]
 })
+
+router.afterEach(route => {
+  document.title = _.template(route.meta.title)(route.params) + ' - Glorified Todo App'
+})
+
+export default router
